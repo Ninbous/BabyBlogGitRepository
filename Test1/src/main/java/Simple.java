9aed8344fabd2dad0cb0c25c17e82f18adb841c1
Ninbous <Nibous@mail.ru>
2017-10-03 18:12:55 +0300

    path chromedriver2

--- Test1/src/main/java/Simple.java
+++ Test1/src/main/java/Simple.java
@@ -11,7 +11,7 @@ public class Simple {
 
     public boolean CreatePostMethod() {
         //System.setProperty("webdriver.chrome.driver", "C:\\SeleniumDrivers\\chromedriver.exe");
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver.tbz2");
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver.tar.bz2");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.babyblog.ru/");
