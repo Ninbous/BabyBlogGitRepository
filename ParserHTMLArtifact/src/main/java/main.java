f1a83e0b1b7dd81913e35b6e31ea8df2ae7adfd5
Ninbous <Nibous@mail.ru>
2018-04-02 23:19:32 +0300

    1

--- ParserHTMLArtifact/src/main/java/main.java
+++ ParserHTMLArtifact/src/main/java/main.java
@@ -14,55 +14,55 @@ public class main {
 
         String[] urls = {
                 //Главная
-                "http://bbnew-1096.testbb.ru/",
-                //КБ
-                "http://bbnew-1096.testbb.ru/cb",
-                "http://bbnew-1096.testbb.ru/cb/1-trimestr",
-                "http://bbnew-1096.testbb.ru/cb/1-mesyac-beremennosti",
-                "http://bbnew-1096.testbb.ru/cb/1-nedelya",
-                "http://bbnew-1096.testbb.ru/cb/snimki-uzi/1-nedelya",
-                "http://bbnew-1096.testbb.ru/cb/foto-zhivotov/1-nedelya",
+             //   "http://bbnew-1096.testbb.ru/",
+             //   //КБ
+             //   "http://bbnew-1096.testbb.ru/cb",
+             //   "http://bbnew-1096.testbb.ru/cb/1-trimestr",
+             //   "http://bbnew-1096.testbb.ru/cb/1-mesyac-beremennosti",
+             //   "http://bbnew-1096.testbb.ru/cb/1-nedelya",
+             //   "http://bbnew-1096.testbb.ru/cb/snimki-uzi/1-nedelya",
+             //   "http://bbnew-1096.testbb.ru/cb/foto-zhivotov/1-nedelya",
                 "http://bbnew-1096.testbb.ru/cb/sovety/1-nedelya/lenta",
                 "http://bbnew-1096.testbb.ru/cb/sovety/1-nedelya/lenta/2",
-                "http://bbnew-1096.testbb.ru/cb/sovety/1-nedelya",
-                "http://bbnew-1096.testbb.ru/cb/sovety/pdelikatnaya-problema-vnbsp1-trimestre-beremennosti-gemorrojjp",
-                "http://bbnew-1096.testbb.ru/cb/sovety/pdelikatnaya-problema-vnbsp1-trimestre-beremennosti-gemorrojjp/3",
-                //Каталог сообществ
-                "http://bbnew-1096.testbb.ru/community",
-                "http://bbnew-1096.testbb.ru/community/group/qqqqq",
-                //Поиск ?q = беременность
-                "http://bbnew-1096.testbb.ru/search/all/?query=%D0%91%D0%B5%D1%80%D0%B5%D0%BC%D0%B5%D0%BD%D0%BD%D0%BE%D1%81%D1%82%D1%8C",
-                "http://bbnew-1096.testbb.ru/search/all/2?query=%D0%91%D0%B5%D1%80%D0%B5%D0%BC%D0%B5%D0%BD%D0%BD%D0%BE%D1%81%D1%82%D1%8C&sort=relevance",
-                //Приложения
-                "http://bbnew-1096.testbb.ru/apps",
+            //   "http://bbnew-1096.testbb.ru/cb/sovety/1-nedelya",
+            //   "http://bbnew-1096.testbb.ru/cb/sovety/pdelikatnaya-problema-vnbsp1-trimestre-beremennosti-gemorrojjp",
+            //   "http://bbnew-1096.testbb.ru/cb/sovety/pdelikatnaya-problema-vnbsp1-trimestre-beremennosti-gemorrojjp/3",
+            //   //Каталог сообществ
+            //   "http://bbnew-1096.testbb.ru/community",
+            //   "http://bbnew-1096.testbb.ru/community/group/qqqqq",
+            //   //Поиск ?q = беременность
+            //   "http://bbnew-1096.testbb.ru/search/all/?query=%D0%91%D0%B5%D1%80%D0%B5%D0%BC%D0%B5%D0%BD%D0%BD%D0%BE%D1%81%D1%82%D1%8C",
+            //   "http://bbnew-1096.testbb.ru/search/all/2?query=%D0%91%D0%B5%D1%80%D0%B5%D0%BC%D0%B5%D0%BD%D0%BD%D0%BE%D1%81%D1%82%D1%8C&sort=relevance",
+            //   //Приложения
+            //   "http://bbnew-1096.testbb.ru/apps",
                 "http://bbnew-1096.testbb.ru/apps/horo",
                 "http://bbnew-1096.testbb.ru/apps/horo/2",
-                //Роддома
-                "http://bbnew-1096.testbb.ru/roddom",
-                "http://bbnew-1096.testbb.ru/roddom/country/3159",
+           //     //Роддома
+           //     "http://bbnew-1096.testbb.ru/roddom",
+           //     "http://bbnew-1096.testbb.ru/roddom/country/3159",
                 "http://bbnew-1096.testbb.ru/roddom/city/4400",
                 "http://bbnew-1096.testbb.ru/roddom/city/4400?page=2",
                 //Страница роддома
                 "http://bbnew-1096.testbb.ru/roddom/info/41",
-                "http://bbnew-1096.testbb.ru/roddom/info/41/services",
+          //      "http://bbnew-1096.testbb.ru/roddom/info/41/services",
                 "http://bbnew-1096.testbb.ru/roddom/info/41/otzyv",
                 "http://bbnew-1096.testbb.ru/roddom/info/41/otzyv?page=2",
-                "http://bbnew-1096.testbb.ru/roddom/info/41/discuss",
+           //     "http://bbnew-1096.testbb.ru/roddom/info/41/discuss",
                 "http://bbnew-1096.testbb.ru/roddom/info/41/list_mothers",
                 "http://bbnew-1096.testbb.ru/roddom/info/41/list_mothers?page=2",
-                "http://bbnew-1096.testbb.ru/roddom/info/41/photos",
-                //Статьи
-                "http://bbnew-1096.testbb.ru/article",
-                "http://bbnew-1096.testbb.ru/article/post/pkak-podobrat-cvet-volos-pravilnop",
-                "http://bbnew-1096.testbb.ru/article/post/pkak-podobrat-cvet-volos-pravilnop/2",
-                //Фотоконкурсы
-                "http://bbnew-1096.testbb.ru/photoconcurs",
-                "http://bbnew-1096.testbb.ru/photoconcurs/past",
-                "http://bbnew-1096.testbb.ru/photoconcurs/archive",
-                "http://bbnew-1096.testbb.ru/photoconcurs/31",
-                "http://bbnew-1096.testbb.ru/photoconcurs/31/page/2",
-                "http://bbnew-1096.testbb.ru/photoconcurs/31/rules",
-                //Прямая речь
+          //      "http://bbnew-1096.testbb.ru/roddom/info/41/photos",
+          //      //Статьи
+          //      "http://bbnew-1096.testbb.ru/article",
+          //      "http://bbnew-1096.testbb.ru/article/post/pkak-podobrat-cvet-volos-pravilnop",
+          //      "http://bbnew-1096.testbb.ru/article/post/pkak-podobrat-cvet-volos-pravilnop/2",
+          //      //Фотоконкурсы
+          //      "http://bbnew-1096.testbb.ru/photoconcurs",
+          //      "http://bbnew-1096.testbb.ru/photoconcurs/past",
+          //      "http://bbnew-1096.testbb.ru/photoconcurs/archive",
+          //      "http://bbnew-1096.testbb.ru/photoconcurs/31",
+          //      "http://bbnew-1096.testbb.ru/photoconcurs/31/page/2",
+          //      "http://bbnew-1096.testbb.ru/photoconcurs/31/rules",
+          //      //Прямая речь
                 "http://bbnew-1096.testbb.ru/all",
                 "http://bbnew-1096.testbb.ru/all/2",
                 "http://bbnew-1096.testbb.ru/all/novice",
@@ -70,51 +70,51 @@ public class main {
                 "http://bbnew-1096.testbb.ru/all/monthlenta/0",
                 "http://bbnew-1096.testbb.ru/all/monthlenta/0/2",
                 //Помощь по сайту
-                "http://bbnew-1096.testbb.ru/help",
-                "http://bbnew-1096.testbb.ru/help/category/1",
-                "http://bbnew-1096.testbb.ru/help/category/2",
-                "http://bbnew-1096.testbb.ru/help/category/5",
-                "http://bbnew-1096.testbb.ru/help/category/8",
-                "http://bbnew-1096.testbb.ru/help/category/31",
-                "http://bbnew-1096.testbb.ru/help/category/39",
-                //Поиск подруг
-                "http://bbnew-1096.testbb.ru/search",
-                "http://bbnew-1096.testbb.ru/search/people/2?search=1&geo=&geo_id=0&name=%D0%A1%D0%B0%D1%88%D0%B0&child_w=0&preg=0&child=0",
-                //КР
-                "http://bbnew-1096.testbb.ru/cr",
-                "http://bbnew-1096.testbb.ru/cr/cr0",
-                "http://bbnew-1096.testbb.ru/cr/sovety/cr0",
+          //     "http://bbnew-1096.testbb.ru/help",
+          //     "http://bbnew-1096.testbb.ru/help/category/1",
+          //     "http://bbnew-1096.testbb.ru/help/category/2",
+          //     "http://bbnew-1096.testbb.ru/help/category/5",
+          //     "http://bbnew-1096.testbb.ru/help/category/8",
+          //     "http://bbnew-1096.testbb.ru/help/category/31",
+          //     "http://bbnew-1096.testbb.ru/help/category/39",
+          //     //Поиск подруг
+          //     "http://bbnew-1096.testbb.ru/search",
+          //     "http://bbnew-1096.testbb.ru/search/people/2?search=1&geo=&geo_id=0&name=%D0%A1%D0%B0%D1%88%D0%B0&child_w=0&preg=0&child=0",
+          //     //КР
+          //     "http://bbnew-1096.testbb.ru/cr",
+          //     "http://bbnew-1096.testbb.ru/cr/cr0",
+          //     "http://bbnew-1096.testbb.ru/cr/sovety/cr0",
                 "http://bbnew-1096.testbb.ru/cr/sovety/cr0/lenta",
                 "http://bbnew-1096.testbb.ru/cr/sovety/cr0/lenta/2",
-                "http://bbnew-1096.testbb.ru/cr/sovety/cr0/pkogda-malysh-dolzhen-nachat-derzhat-golovku-samostoyatelnop",
-                "http://bbnew-1096.testbb.ru/cr/sovety/cr0/pkogda-malysh-dolzhen-nachat-derzhat-golovku-samostoyatelnop/3",
+           //     "http://bbnew-1096.testbb.ru/cr/sovety/cr0/pkogda-malysh-dolzhen-nachat-derzhat-golovku-samostoyatelnop",
+           //     "http://bbnew-1096.testbb.ru/cr/sovety/cr0/pkogda-malysh-dolzhen-nachat-derzhat-golovku-samostoyatelnop/3",
                 "http://bbnew-1096.testbb.ru/cr/gallery/cr0",
-                "http://bbnew-1096.testbb.ru/cr/checklists/cr0",
+           //     "http://bbnew-1096.testbb.ru/cr/checklists/cr0",
                 //Вопрос-Ответы
                 "http://bbnew-1096.testbb.ru/questions",
                 "http://bbnew-1096.testbb.ru/questions/new/2",
-                "http://bbnew-1096.testbb.ru/questions/id1751365/64217",
-                "http://bbnew-1096.testbb.ru/questions/id1751365/64217/3",
+           //     "http://bbnew-1096.testbb.ru/questions/id1751365/64217",
+           //     "http://bbnew-1096.testbb.ru/questions/id1751365/64217/3",
                 //Тестдрайвы
                 "http://bbnew-1096.testbb.ru/td/0",
                 "http://bbnew-1096.testbb.ru/td/0/active",
                 "http://bbnew-1096.testbb.ru/td/0/close",
                 "http://bbnew-1096.testbb.ru/td/0?page=2",
                 "http://bbnew-1096.testbb.ru/td/info/34",
-                "http://bbnew-1096.testbb.ru/td/itog/34",
-                "http://bbnew-1096.testbb.ru/td/rules/34",
-                "http://bbnew-1096.testbb.ru/td/11",
+           //     "http://bbnew-1096.testbb.ru/td/itog/34",
+           //     "http://bbnew-1096.testbb.ru/td/rules/34",
+           //     "http://bbnew-1096.testbb.ru/td/11",
                 //Сообщества
                 "http://bbnew-1096.testbb.ru/community/lenta/conception",
                 "http://bbnew-1096.testbb.ru/community/lenta/conception/15829",
                 "http://bbnew-1096.testbb.ru/community/rules/conception",
                 "http://bbnew-1096.testbb.ru/community/lenta_cat/conception/16526",
-                "http://bbnew-1096.testbb.ru/community/post/conception/1715598",
+            //    "http://bbnew-1096.testbb.ru/community/post/conception/1715598",
                 //Дневники
                 "http://bbnew-1096.testbb.ru/user/lenta/ashlyy",
-                "http://bbnew-1096.testbb.ru/user/lenta/ashlyy/5",
-                "http://bbnew-1096.testbb.ru/user/info/friends/ashlyy",
-                "http://bbnew-1096.testbb.ru/user/info/ashlyy"
+               "http://bbnew-1096.testbb.ru/user/lenta/ashlyy/5",
+            //    "http://bbnew-1096.testbb.ru/user/info/friends/ashlyy",
+            //    "http://bbnew-1096.testbb.ru/user/info/ashlyy"
         };
 
         for(int i = 0; i<urls.length;i++) {
@@ -182,7 +182,7 @@ public class main {
             for (Element aElem : metaDescription) {
                 DescriptionContent = aElem.attr("content");
             }
-            for (Element aElem : linkPrev) {
+            for (Element aElem : linkNext) {
                 linkNextHref = aElem.attr("href");
             }
             for (Element aElem : linkPrev) {
@@ -236,10 +236,10 @@ class Page {
 
     @Override
     public String toString() {
-        return "Page{" +
-                "descriptionContent='" + descriptionContent + '\'' +
-                ", linkNext='" + linkNext + '\'' +
-                ", linkPrev='" + linkPrev + '\'' +
+        return "Page{" + "\n" +
+                "descriptionContent='" + descriptionContent + '\'' + "\n" +
+                "linkNext='" + linkNext + '\'' + "\n" +
+                "linkPrev='" + linkPrev + '\'' + "\n" +
                 '}';
     }
 }
